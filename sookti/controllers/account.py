af36865ff1dbbcf92b4211229ca206234b19025d
Pradeep Gowda <btbytes@users.noreply.github.com>
2007-03-28 11:59:58 +0000

    Fixed Login problems

--- sookti/controllers/account.py
+++ sookti/controllers/account.py
@@ -4,7 +4,7 @@ from sookti.models import *
 from sookti.lib.base import *
 from sookti.lib.database import session_context
 from logging import basicConfig, DEBUG, INFO, debug, info, warning, error, critical
-basicConfig(level=DEBUG, format="%(levelname)s:%(module)s: %(message)s") # default WARNING
+basicConfig(level=DEBUG, format="%(levelname)s:%(module)s: %(message)s")
 from authkit.pylons_adaptors import authorize
 from authkit.permissions import RemoteUser
 from pylons.decorators import rest
@@ -12,7 +12,7 @@ from pylons.decorators import rest
 class AccountController(BaseController):
     def __before__(self):
         self.session = session_context.current
-        self.session.clear()            # remove stale info
+        self.session.clear()
         
     def index(self):
         return Response('')
@@ -24,18 +24,17 @@ class AccountController(BaseController):
         c.message = "You have been signed out."
         if request.environ.has_key('REMOTE_USER'):
             del request.environ['REMOTE_USER']
-        return render_response('mako',"/account_signout.mak")
-        
-    # Want to use "signin" and distinguish by POST
-    # I put a valid() in app_globals, but not using it now...
-    # should move the lastlogin thing there if we gut this?
+        return render_response('mako',"/account_signout.mak")        
+    
     def signin_check(self):
         username = request.params.get('username', '').strip().lower()
         password = request.params.get('password', '').strip()        
-        users = self.session.query(User).select_by(username=username)        
         hash = sha.new()
         hash.update(password)        
-        password_digest = hash.digest()
+        password_digest = hash.digest()        
+        users = self.session.query(User).select_by(
+            username=username, 
+            password=password_digest)                
         
         if len(users) > 1:
             c.message="More than one user in database with username=%s found" % username
@@ -43,13 +42,8 @@ class AccountController(BaseController):
         elif not users:
             c.message="No such user %s" % username
             return render_response('mako',"/account_signin.mak")
-        user = users[0]
-        print '-'*80, 'password', password_digest, 'user.password', user.password        
-        
-        if password_digest != user.password:
-            c.message="Bad password=%s for username=%s" % (password, username)
-            return render_response('mako',"/account_signin.mak")
             
+        user = users[0]        
         request.environ['paste.auth_tkt.set_user'](username)
         user.lastlogin = datetime.now()
         self.session.save(user)
